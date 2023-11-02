48289c8c73c46e74518f742f5a3ca806021b65f6
eiki_obara <eiki_obara@yrglm.co.jp>
2023-11-02 18:02:02 +0900

    [bugfix]: declaration bug
    I cannot understand how to use declaration,
    so I remove it...(wanna support someday)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,2 @@
-declare module "hello" {
-    export const CONSTANT_VALUE: string;
-    export function hello(word?: string);
-}
+export const CONSTANT_VALUE: string;
+export function hello(word?: string);
