bab052aa2fe9fdc2a85e2a0e132b98cbef93d2e1
eiki_obara <eiki_obara@yrglm.co.jp>
2023-11-02 17:53:44 +0900

    [update]: implement with esmodule
    change commonjs to esmodule

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,6 @@
-const CONSTANT_VALUE = "CONSTANT_VALUE";
+export const CONSTANT_VALUE = "CONSTANT_VALUE";
 
-function hello(word) {
+export function hello(word) {
     if (word === void 0) { word = "TypeScript"; }
     return "Hello, ".concat(word);
 }
-
-module.exports = {
-    CONSTANT_VALUE: CONSTANT_VALUE,
-    hello: hello,
-};
